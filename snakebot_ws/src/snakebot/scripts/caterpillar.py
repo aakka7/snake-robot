76c2fdd7379e361f9e658c703f9765955f43760c
aakka7 <aakkaped@gmail.com>
2025-05-03 22:36:36 -0400

    Fixed path to access libraries

--- snakebot_ws/src/snakebot/scripts/caterpillar.py
+++ snakebot_ws/src/snakebot/scripts/caterpillar.py
@@ -8,6 +8,7 @@ import sys, select, termios, tty
 import math
 import numpy as np
 
+import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from custom_libs.gait import Gait
 
--- snakebot_ws/src/snakebot/scripts/forward.py
+++ snakebot_ws/src/snakebot/scripts/forward.py
@@ -6,6 +6,7 @@ from std_msgs.msg import Float64
 import sys, select, termios, tty
 import math
 
+import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from custom_libs.gait import Gait
 
